4a749d6a74169db3c012f84e262dbda6c43ec55f
Ram Prasad Poudal <ramprasad8314@gmail.com>
2020-05-07 12:26:47 -0500

    Changes to codonTranslator.cs - Added code to translate the provided codon sequences into their corresponding proteins based on the mappings provided in the data file packages.config - Added Newtonsoft.Json

--- InterviewQuestions/CodonTranslator.cs
+++ InterviewQuestions/CodonTranslator.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using System.Xml;
 
 namespace InterviewQuestions
 {
@@ -37,6 +42,7 @@ namespace InterviewQuestions
 
     public class CodonTranslator
     {
+        private Dictionary<string, string> codonMap;
 
         /// <summary>
         /// Constructor
@@ -44,6 +50,7 @@ namespace InterviewQuestions
         /// <param name="codonTableFileName">Filename of the DNA codon table.</param>
         public CodonTranslator(string codonTableFileName)
         {
+            codonMap = new Dictionary<string, string>();
             var file = new StreamReader(codonTableFileName);
             var fileContent = file.ReadToEnd();
             BuildTranslationMapFromFileContent(fileContent, Path.GetExtension(codonTableFileName));
@@ -51,7 +58,19 @@ namespace InterviewQuestions
 
         private void BuildTranslationMapFromFileContent(string fileContent, string fileType)
         {
-            throw new System.NotImplementedException(string.Format("The contents of the file with type \"{0}\" have been loaded, please make use of it.\n{1}",fileType,fileContent));
+            switch (fileType)
+            {
+                case ".xml":
+                    BuildMapFromXml(fileContent);
+                    break;
+                case ".json":
+                    BuildMapFromJson(fileContent);
+                    break;
+                case ".csv":
+                    BuildMapFromCsv(fileContent);
+                    break;
+                default: throw new System.NotImplementedException(string.Format("The contents of the file with type \"{0}\" have been loaded, please make use of it.\n{1}", fileType, fileContent));
+            }
         }
 
         /// <summary>
@@ -61,7 +80,156 @@ namespace InterviewQuestions
         /// <returns>Amino acid sequence</returns>
         public string Translate(string dna)
         {
-            return "";
+            StringBuilder sb = new StringBuilder();
+
+            char[] dnaChars = dna.ToCharArray();
+
+            var pointer = 0;
+            bool isStartFound = false;
+
+            while (pointer < dnaChars.Length)
+            {
+                var nucleotidesSb = new StringBuilder();
+                int i = 0;
+
+                while (i < 3)
+                {
+                    if (pointer < dnaChars.Length)
+                    {
+                        nucleotidesSb.Append(dnaChars[pointer++]);
+                    }
+
+                    i++;
+                }
+
+                var nucleotides = nucleotidesSb.ToString();
+
+                if (codonMap.ContainsKey(nucleotides))
+                {
+                    var codon = codonMap[nucleotides];
+
+                    if (codon.Contains("#"))
+                    {
+                        var codonValArr = codon.Split('#');
+                        if (codonValArr[1].Equals("START", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            isStartFound = true;
+                            sb.Append(codonValArr[0]);
+                        }
+                    }
+                    else if (codon.Equals("STOP", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        break;
+                    }
+                    else if (isStartFound)
+                    {
+                        sb.Append(codon);
+                    }
+                }
+            }
+
+            return sb.ToString();
         }
+
+        #region Helpers
+
+        public class Codon
+        {
+            public List<string> Starts { get; set; }
+            public List<string> Stops { get; set; }
+
+            public List<CodonMap> CodonMap { get; set; }
+        }
+        public class CodonMap
+        {
+            public string Codon { get; set; }
+            public string AminoAcid { get; set; }
+
+        }
+
+        private void BuildMapFromXml(string fileContent)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(fileContent);
+
+            var codonsXpath = "Data/CodonMap/CodonPair";
+            var codonNodes = xmlDoc.SelectNodes(codonsXpath);
+
+            foreach (XmlNode codonNode in codonNodes)
+            {
+                var codon = codonNode.FirstChild.FirstChild.Value;
+                var aminoAcid = codonNode.LastChild.LastChild.Value;
+
+                codonMap.Add(codon, aminoAcid);
+            }
+
+            var startsXpath = "Data/Starts";
+            var startsNodes = xmlDoc.SelectNodes(startsXpath);
+            foreach (XmlNode startNode in startsNodes)
+            {
+                var codon = startNode.FirstChild.FirstChild.Value;
+                if (codonMap.ContainsKey(codon))
+                {
+                    var codonVal = codonMap[codon];
+                    var valueToReplace = codonVal + "#START";
+                    codonMap[codon] = valueToReplace;
+                }
+            }
+
+            var stopsXpath = "Data/Stops";
+            var stopsNodes = xmlDoc.SelectNodes(stopsXpath);
+            foreach (XmlNode stopNode in stopsNodes)
+            {
+                var codon = stopNode.FirstChild.FirstChild.Value;
+                codonMap.Add(codon, "STOP");
+            }
+        }
+
+        private void BuildMapFromJson(string fileContent)
+        {
+            var cObj = JsonConvert.DeserializeObject<Codon>(fileContent);
+
+            foreach (var codon in cObj.CodonMap)
+            {
+                codonMap.Add(codon.Codon, codon.AminoAcid);
+            }
+
+            foreach (var codon in cObj.Stops)
+            {
+                codonMap.Add(codon, "STOP");
+            }
+
+            foreach (var codon in cObj.Starts)
+            {
+                if (codonMap.ContainsKey(codon))
+                {
+                    var codonVal = codonMap[codon];
+                    var valueToReplace = codonVal + "#START";
+                    codonMap[codon] = valueToReplace;
+                }
+            }
+        }
+
+        private void BuildMapFromCsv(string fileContent)
+        {
+            string[] file = fileContent.Split(new[] { "\n" }, StringSplitOptions.None);
+
+            foreach (var line in file)
+            {
+                string[] current = line.Split(',');
+
+                if (codonMap.ContainsKey(current[0]))
+                {
+                    var currentVal = codonMap[current[0]];
+                    var valueToReplace = currentVal + "#" + current[1];
+                    codonMap[current[0]] = valueToReplace;
+                }
+                else
+                {
+                    codonMap.Add(current[0], current[1]);
+                }
+            }
+        }
+        #endregion
     }
 }
