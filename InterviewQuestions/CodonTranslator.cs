f42361ea310bcfb9fa62bb8768d88033f528c2b4
Jared Gabaldon <jaredgabaldon25@gmail.com>
2020-11-04 12:56:49 -0700

    updated files for codontest

--- InterviewQuestions/CodonTranslator.cs
+++ InterviewQuestions/CodonTranslator.cs
@@ -1,39 +1,12 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
 
 namespace InterviewQuestions
 {
-    /*  
-     *  The genetic code is a set of rules by which DNA or mRNA is translated into proteins (amino acid sequences).
-     *  
-     *  1) Three nucleotides (or tri-nucleotide), called codons, specify which amino acid will be used.
-     *  2) Since codons are defined by three nucleotides, every sequence can be read in three reading frames, depending on the starting point.
-     *     The actual reading frame used for translation is determined by a start codon. 
-     *     In our case, we will define the start codon to be the most commonly used ATG (in some organisms there may be other start codons).
-     *  3) Translation begins with the start codon, which is translated as Methionine (abbreviated as 'M').
-     *  4) Translation continues until a stop codon is encountered. There are three stop codons (TAG, TGA, TAA)
-     *  
-     * 
-     *  Included in this project is a comma seperated value (CSV) text file with the codon translations.
-     *  Each line of the file has the codon, followed by a space, then the amino acid (or start or stop)
-     *  For example, the first line:
-     *  CTA,L
-     *  should be interpreted as: "the codon CTA is translated to the amino acid L"
-     *  
-     *  
-     *  You should not assume that the input sequence begins with the start codon. Any nucleotides before the start codon should be ignored.
-     *  You should not assume that the input sequence ends with the stop codon. Any nucleotides after the stop codon should be ignored.
-     * 
-     *  For example, if the input DNA sequence is GAACAAATGCATTAATACAAAAA, the output amino acid sequence is MH.
-     *  GAACAA ATG CAT TAA TACAAAAA
-     *         \ / \ /
-     *          M   H
-     *          
-     *  ATG -> START -> M
-     *  CAT -> H
-     *  TAA -> STOP
-     *  
-     */
-
 
     public class CodonTranslator
     {
@@ -41,17 +14,24 @@ namespace InterviewQuestions
         /// <summary>
         /// Constructor
         /// </summary>
-        /// <param name="codonTableFileName">Filename of the DNA codon table.</param>
+        /// <param name="CodonTable.csv>Filename of the DNA codon table.</param>
         public CodonTranslator(string codonTableFileName)
         {
-            var file = new StreamReader(codonTableFileName);
+            var file = new StreamReader("CodonTable.csv");
             var fileContent = file.ReadToEnd();
             BuildTranslationMapFromFileContent(fileContent, Path.GetExtension(codonTableFileName));
         }
-
         private void BuildTranslationMapFromFileContent(string fileContent, string fileType)
         {
-            throw new System.NotImplementedException(string.Format("The contents of the file with type \"{0}\" have been loaded, please make use of it.\n{1}",fileType,fileContent));
+            List<string> filecont = fileContent.Split(',').ToList<string>();
+            Dictionary<string, string> Codon_Dict = new Dictionary<string, string>();
+
+            for (int line = 0; line < fileContent.Length; line++)
+            {
+                //Codon_Dict.Add(line[0], line[1]);
+            }
+
+                throw new System.NotImplementedException(string.Format("The contents of the file with type \"{0}\" have been loaded, please make use of it.\n{1}", fileType, fileContent));
         }
 
         /// <summary>
@@ -62,6 +42,93 @@ namespace InterviewQuestions
         public string Translate(string dna)
         {
             return "";
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            string dna = "ATGCATTAA";
+
+            Dictionary<string, string> Codon_Dict = Read_Csv_Return_Dict();
+
+            int start_index = dna.IndexOf("ATG");
+            int end_index = FindEndingIndex(dna);
+
+            string dna_key = "";
+            for (int dna_index = start_index; dna_index < end_index; dna_index = dna_index + 3)
+            {
+                string codon = dna.Substring(dna_index, 3);
+                string codon_key = Codon_Dict[codon];
+
+                if (codon_key == "Start")
+                {
+                    codon_key = "M";
+                }
+
+                dna_key = dna_key + codon_key;
+            }
+
+            Console.WriteLine("result: " + dna_key);
+
+        }
+
+        public static int FindEndingIndex(string dna)
+        {
+            int taa_index = dna.IndexOf("TAA");
+            int tag_index = dna.IndexOf("TAG");
+            int tga_index = dna.IndexOf("TGA");
+
+            if (taa_index < 0)
+            {
+                taa_index = 1000;
+            }
+            if (tag_index < 0)
+            {
+                tag_index = 1000;
+            }
+            if (tga_index < 0)
+            {
+                tga_index = 1000;
+            }
+
+            int[] array1 = new int[] { taa_index, tag_index, tga_index };
+
+            int end_index = array1.Min();
+
+            return end_index;
+        }
+
+
+        public static Dictionary<string, string> Read_Csv_Return_Dict()
+        {
+            Dictionary<string, string> Codon_Dict = new Dictionary<string, string>();
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("CodonTable.csv"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        string[] col = line.Split(',');
+
+                        Codon_Dict.Add(col[0], col[1]);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return Codon_Dict;
         }
     }
 }
